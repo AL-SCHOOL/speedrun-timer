256b6b74f7fb60853a403a0f7f34f907176c6acc
AL_SCHOOL <61769442+AL-SCHOOL@users.noreply.github.com>
2021-01-26 11:36:32 -0500

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,19 +1,14 @@
-function darkMode() {
-  document.body.style.background = "#1a1a1a";
-  document.getElementById("logo").style.color = "#ffffff";
-  document.getElementById("FullTimer").style.backgroundColor = "#ffffff";
-  document.getElementById("FullInput").style.backgroundColor = "#ffffff";
-}
-
-  
-function lightMode() {
-  document.body.style.background = "#ffffff";
-  document.getElementById("logo").style.color = "#000000";
-  document.getElementById("FullTimer").style.backgroundColor = "#000000";
-  document.getElementById("FullInput").style.backgroundColor = "#000000";
-}
-
 function switchMode() {
   onOff = document.getElementById("sliderAnswer");
-  if (onOff.checked) {darkMode();}
-  else {lightMode();}
+  if (onOff.checked) {
+    document.body.style.background = "#1a1a1a";
+    document.getElementById("logo").style.color = "#ffffff";
+    document.getElementById("FullTimer").style.backgroundColor = "#ffffff";
+    document.getElementById("FullInput").style.backgroundColor = "#ffffff";
+  }
+  else {
+    document.body.style.background = "#ffffff";
+    document.getElementById("logo").style.color = "#000000";
+    document.getElementById("FullTimer").style.backgroundColor = "#000000";
+    document.getElementById("FullInput").style.backgroundColor = "#000000";
+  }
