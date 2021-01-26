1f53d8859fe553aba0301f45a07a6ddf6c29f501
AL_SCHOOL <61769442+AL-SCHOOL@users.noreply.github.com>
2021-01-25 22:53:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function SetTimer() {
+  // Receive data
+  var title = document.getElementById("Title").value;
+  var split1 = document.getElementById("Slot1").value;
+  var split2 = document.getElementById("Slot2").value;
+  var split3 = docuemnt.getElementById("Slot3").value;
+  var split4 = document.getElementById("Slot4").value;
+  var split5 = document.getElementById("Slot5").value;
+  var split6 = document.getElementById("Slot6").value;
+  var split7 = document.getElementById("Slot7").value;
+  var split8 = document.getElementById("Slot8").value;
+  var split9 - document.getElementById("Slot9").value;
+  // Write data
 }
