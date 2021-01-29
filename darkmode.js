fea1f09cdf7c481522189ee2db8bc7600ea1f13a
AL_SCHOOL <61769442+AL-SCHOOL@users.noreply.github.com>
2021-01-29 08:32:01 -0500

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,5 +1,7 @@
+var onOff = document.getElementById("sliderAnswer");
+var buttonPress = 0;
+
 function switchMode() {
-  var onOff = document.getElementById("sliderAnswer");
   if (onOff.checked = true) {
     document.body.style.background = "#1a1a1a";
     document.getElementById("logo").style.color = "#ffffff";
@@ -14,15 +16,15 @@ function switchMode() {
   }
 
   function switchModeBeta() {
-    var buttonPress += 1;
-    if (buttonPress == 1) {
+    if (buttonPress == 0) {
+      buttonPress = 1;
       document.body.style.background = "#1a1a1a";
       document.getElementById("logo").style.color = "#ffffff";
       document.getElementById("FullTimer").style.backgroundColor = "#ffffff";
       document.getElementById("FullInput").style.backgroundColor = "#ffffff";
     }
     else {
-      button = 0;
+      buttonPress = 0;
       document.body.style.background = "#ffffff";
       document.getElementById("logo").style.color = "#000000";
       document.getElementById("FullTimer").style.backgroundColor = "#000000";
