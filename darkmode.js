a471c0877b5f6c67ab5928cb12a00b44ebb06223
AL_SCHOOL <61769442+AL-SCHOOL@users.noreply.github.com>
2021-01-29 09:48:20 -0500

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -4,14 +4,14 @@ function switchMode() {
   if (onOff.checked) {
     document.body.style.background = "#1a1a1a";
     document.getElementById("logo").style.color = "#ffffff";
-    document.getElementById("ModeNames").style.color = "#ffffff;
+    document.getElementById("ModeNames").style.color = "#ffffff";
     document.getElementById("FullTimer").style.backgroundColor = "#ffffff";
     document.getElementById("FullInput").style.backgroundColor = "#ffffff";
   }
   else {
     document.body.style.background = "#ffffff";
     document.getElementById("logo").style.color = "#000000";
-    document.getElementById("ModeNames").style.color = "#000000;
+    document.getElementById("ModeNames").style.color = "#000000";
     document.getElementById("FullTimer").style.backgroundColor = "#000000";
     document.getElementById("FullInput").style.backgroundColor = "#000000";
   }
